2a293da7df221406d5f557b3adde975f21a5cfce
brandon-l07 <brandonlu021@gmail.com>
2025-06-01 19:03:29 -0700

    Update script.js

--- script.js
+++ script.js
@@ -114,11 +114,11 @@ function shuffle(array) {
 }
     // Your playlist - add your own songs here
     const playlist = [
-      '/Users/newuser/Documents/Music-Site/Abba - Dancing Queen (Official Music Video Remastered).mp3',
-      '/Users/newuser/Documents/Music-Site/Rick Astley - Together Forever (Official Video) [4K Remaster].mp3',
-      '/Users/newuser/Documents/Music-Site/Dschinghis Khan - Moskau (Starparade 14.06.1979).mp3',
-      '/Users/newuser/Documents/Music-Site/Redbone - Come and Get Your Love (Single Edit - Audio).mp3',
-      '/Users/newuser/Documents/Music-Site/Earth, Wind & Fire - September.mp3'
+      'Music-Site/Abba - Dancing Queen (Official Music Video Remastered).mp3',
+      'Music-Site/Rick Astley - Together Forever (Official Video) [4K Remaster].mp3',
+      'Music-Site/Dschinghis Khan - Moskau (Starparade 14.06.1979).mp3',
+      'Music-Site/Redbone - Come and Get Your Love (Single Edit - Audio).mp3',
+      'Music-Site/Earth, Wind & Fire - September.mp3'
     ];
 
 shuffle(playlist); 
